fd03f09622c93fab14d0c8b24898688eb1e959ea
Stas Makhno <stanislavcheg@mail.ru>
2017-09-24 21:42:34 +0300

    carrierwave initializer updated

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,7 +8,5 @@ CarrierWave.configure do |config|
   }
 
   config.cache_dir = "#{Rails.root}/tmp/uploads"
-  config.fog_directory    = ENV['S3_BUCKET_NAME']
-  config.s3_access_policy = :public_read
-  config.fog_host         = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
+  config.fog_directory = ENV['S3_BUCKET_NAME']
 end
