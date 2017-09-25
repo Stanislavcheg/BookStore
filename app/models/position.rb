ec297a155ee71711049e884cb52fdb27e2135f22
Stas Makhno <stanislavcheg@mail.ru>
2017-09-25 14:03:01 +0300

    popular sort fixted

--- app/models/position.rb
+++ app/models/position.rb
@@ -4,7 +4,7 @@ class Position < ApplicationRecord
 
   validates :quantity, presence: true, numericality: { greater_than: 0 }
 
-  before_save :finalize
+  before_save :finalize, :set_sold
 
   def unit_price
     if persisted?
@@ -23,5 +23,9 @@ class Position < ApplicationRecord
   def finalize
     self[:unit_price] = unit_price
     self[:total_price] = quantity * self[:unit_price]
+  end
+
+  def set_sold
+    book.update_attributes(sold: book.sold_count)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170918072242) do
+ActiveRecord::Schema.define(version: 20170925105615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 20170918072242) do
     t.float "height"
     t.float "width"
     t.float "depth"
+    t.integer "sold", default: 0
     t.index ["category_id"], name: "index_books_on_category_id"
   end
 
