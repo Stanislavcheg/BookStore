d5bec422dc5ff830e1bcbe2ca3444c65dfdaae09
Stas Makhno <stanislavcheg@mail.ru>
2017-09-27 13:01:25 +0300

    settings feature tests added

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -27,7 +27,7 @@ Capybara.register_driver :headless_chrome do |app|
     desired_capabilities: capabilities
 end
 
-Capybara.javascript_driver = :headless_chrome
+Capybara.javascript_driver = :chrome
 
 Capybara.default_max_wait_time = 10
 
